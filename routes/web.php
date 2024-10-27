83bf3b25e4d8a308137a792dd763c744c2109916
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2024-10-27 12:22:11 +0100

    poprawiony web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/kontakt', function () {
+    return view('kontakt');
+});
+
+Route::get('/onas', function () {
+    $zadania = [
+        'Zadanie 1',
+        'Zadanie 2',
+        'Zadanie 3'
+    ];
+    return view('test.onas',[ 'zadania' => $zadania]);
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
