aa2be7070e51963a67065902aa6ab041ac8a2479
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2024-10-27 12:21:27 +0100

    instalacja Breeze do authentykacji

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,15 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/kontakt', function () {
-    return view('kontakt');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/onas', function () {
-    $zadania = [
-        'Zadanie 1',
-        'Zadanie 2',
-        'Zadanie 3'
-    ];
-    return view('test.onas',[ 'zadania' => $zadania]);
-});
+require __DIR__.'/auth.php';
