1c574cabceeb6b4440da81ade7e8ea04c0cdc170
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2024-10-27 13:47:46 +0100

    grupowanie kontrolera Ogolne

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ use Illuminate\Support\Facades\Route;
 /* Route::get('/', function () {
     return view('ogolne.welcome');
 })->name('start'); */
-Route::get('/', [OgolneController::class,'start'])->name('start');
+//Route::get('/', [OgolneController::class,'start'])->name('start');
 
 /* Route::get('/kontakt', function () {
     return view('ogolne.kontakt');
 })->name('kontakt'); */
-Route::get('/kontakt', [OgolneController::class,'kontakt'])->name('kontakt');
+//Route::get('/kontakt', [OgolneController::class,'kontakt'])->name('kontakt');
 
 /* Route::get('/onas', function () {
     $zadania = [
@@ -33,7 +33,13 @@ Route::get('/kontakt', [OgolneController::class,'kontakt'])->name('kontakt');
     ];
     return view('ogolne.onas',[ 'zadania' => $zadania]);
 })->name('onas'); */
-Route::get('/onas', [OgolneController::class,'onas'])->name('onas');
+//Route::get('/onas', [OgolneController::class,'onas'])->name('onas');
+
+Route::controller(OgolneController::class)->group(function(){
+    Route::get('/', 'start')->name('start');
+    Route::get('/kontakt', 'kontakt')->name('kontakt');
+    Route::get('/onas', 'onas')->name('onas');
+});
 
 Route::get('/dashboard', function () {
     //return view('dashboard');
