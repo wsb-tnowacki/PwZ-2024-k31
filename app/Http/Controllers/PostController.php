8ab60f357a4b75a6143ab5557e82254c43b73b4f
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2024-11-24 14:10:18 +0100

    Walidacja formularzy przez PostStoreRequest

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostStoreRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -27,7 +28,7 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostStoreRequest $request)
     {
         $post = new Post();
 /*         $post->tytul = $request['tytul'];
@@ -35,12 +36,12 @@ class PostController extends Controller
         $post->email = request('email');
         $post->tresc = request('tresc');
         $post->save(); */
-        $request->validate([
+/*         $request->validate([
             'tytul' => 'required|min:3|max:200',
             'autor' => ['required','min:4','max:100'],
             'email' => ['required','email:rfc,dns','max:200'],
             'tresc' => ['required','min:5']
-        ]);
+        ]); */
         $post->create($request->all());
         return redirect()->route('post.index')->with('message','Dodano poprawnie post');
     }
@@ -64,7 +65,9 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
+ //   public function update(Request $request, Post $post)
+    public function update(PostStoreRequest $request, Post $post)
+
     {
         $post->update($request->all());
         return redirect()->route('post.index')->with('message','Zmieniono poprawnie post');
---
+++ app/Http/Requests/PostStoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'tytul' => 'required|min:3|max:200',
+            'autor' => ['required','min:4','max:100'],
+            'email' => ['required','email:rfc,dns','max:200'],
+            'tresc' => ['required','min:5']
+            
+        ];
+    }
+}
